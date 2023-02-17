b221c920aa8fb7978e28d574f9272259ac0fe355
papa <learninpasv@gmail.com>
2023-02-16 20:20:00 -0500

    1 commit/3 it

---
+++ src/specs/market.test.js
@@ -0,0 +1,28 @@
+//const axios = require('axios');
+import axios from 'axios'
+import { assert } from 'chai'
+
+describe('price data', () => {
+    let data;
+
+    before(async () => {
+        await axios.get('https://api-pub.bitfinex.com/v2/ticker/tBTCUSD')
+            .then((response) => {
+                data = response;
+            });
+    });
+
+    it('has 200 response code', () => {
+        assert.equal(data.status, 200, 'the response code is not 200');
+    });
+
+    it('contains 10 values', () => {
+        assert.equal(data.data.length, 10, 'number of values is not 10');
+    });
+
+    it('values should be numbers', () => {
+        for (const value of data.data) {
+            assert.isNumber(value, `value '${value}' is not a number`);
+        }
+    });
+});
